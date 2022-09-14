827b2b390f230c0851ad0fec142f270207e3d561
Javier Tinoco Diaz <jtinocod@yahoo.com>
2022-09-14 01:23:18 -0500

    Initial Commit

---
+++ GitProjectIF/GitProjectIFApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectIFApp.swift
+//  GitProjectIF
+//
+//  Created by Javier Tinoco Diaz on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectIFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
